1eac735ef2a2bdd802d2843a097202a6c89003c5
Regan Svoboda <regansvoboda@gmail.com>
2023-03-21 09:49:52 -0500

    update

--- lib/company.py
+++ lib/company.py
@@ -5,16 +5,12 @@ class Company:
     def __init__ (self, name, founding_year):
         self.name = name
         self.founding_year = founding_year
-        
-
-    def give_freebie(self, freebie):
-
-        pass
 
     def freebies(self):
         return_list = []
         for i in Freebie.all():
-            if i.company == self.name:
+            print(Freebie.instances[0])
+            if i.company == self:
                 return_list.append(i)
         return return_list
     
--- lib/dev.py
+++ lib/dev.py
@@ -4,3 +4,9 @@ class Dev:
     
     def __init__(self, name):
         self.name = name
+
+    def freebie(self):
+        freebie_dev = []
+        for i in Freebie.instances:
+            if i.dev == self.name:
+                pass
